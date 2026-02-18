4baa025588696f51bceee559f8a73b1e3c599bde
Duncan William Saul <saul.dw90@gmail.com>
2026-02-18 15:25:54 +0100

    removed breadcrums from navbar

--- packages/ui/src/layout/Navbar.tsx
+++ packages/ui/src/layout/Navbar.tsx
@@ -11,7 +11,6 @@ import type { SetStateAction, Dispatch } from 'react'
 
 export interface I_NavBarProps {
     items: I_Link[]
-    breadcrumbs?: I_BreadcrumbItem[]
     onNavigate?: (link: I_Link) => void
     scrollLock: Dispatch<SetStateAction<boolean>>
 }
