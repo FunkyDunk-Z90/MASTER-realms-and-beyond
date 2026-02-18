ea93592bf6f7c2ac0a85bdfc1bbea0440b5a859a
Duncan William Saul <saul.dw90@gmail.com>
2026-02-18 15:21:31 +0100

    moved all comp types to ui

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,6 +3,4 @@ export * from './globalTypes'
 export * from './models/users/userIndexTypes'
 export * from './models/users/personalData/personalDataTypesIndex'
 
-export * from './components/indexComponentTypes'
-
 export * from './models/ttrpg/ttrpgTypesIndex'
