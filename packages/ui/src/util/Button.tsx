f32331e93ede561d4673c70601dd3c55bbb9c96c
Duncan William Saul <saul.dw90@gmail.com>
2026-02-18 11:16:28 +0100

    added btn component

--- packages/ui/src/util/Button.tsx
+++ packages/ui/src/util/Button.tsx
@@ -1,7 +1,52 @@
-export default function Button() {
+import { RefObject, MouseEvent, ChangeEvent } from 'react'
+
+type T_ButtonEvent =
+    | MouseEvent<HTMLButtonElement>
+    | ChangeEvent<HTMLButtonElement>
+
+type T_BtnTypes = 'button' | 'reset' | 'submit'
+
+type T_BtnTheme =
+    | 'accent'
+    | 'warning'
+    | 'danger'
+    | 'submit'
+    | 'success'
+    | 'disabled'
+
+type T_BtnRef = RefObject<HTMLButtonElement>
+
+interface I_ButtonProps {
+    children: React.ReactNode
+    handleClick?: (e?: T_ButtonEvent | undefined) => void
+    isDisabled?: boolean
+    theme?: T_BtnTheme
+    btnType?: T_BtnTypes
+    withRef?: T_BtnRef
+}
+
+export default function Button({
+    children,
+    handleClick,
+    isDisabled,
+    theme,
+    btnType,
+    withRef,
+}: I_ButtonProps) {
+    const btnTheme = !theme ? '' : theme
+    const defaultBtnType = !btnType ? 'button' : btnType
+
     return (
-        <div>
-            <h1>Button</h1>
-        </div>
+        <button
+            suppressHydrationWarning
+            onClick={handleClick}
+            disabled={isDisabled}
+            className={`btn ${!isDisabled ? btnTheme : 'disabled'}`}
+            type={defaultBtnType}
+            ref={withRef}
+            role="button"
+        >
+            {children}
+        </button>
     )
 }
